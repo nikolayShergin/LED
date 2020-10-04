05c69d0ece4ec6a128f040929f2097db02334ca5
nikolayShergin <nikolay.shergin@gmail.com>
2020-10-04 14:55:48 +0200

    m-pins doesnt work

--- ver1.0.cells.py
+++ ver1.0.cells.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template, url_for
 from markupsafe import escape
-import datetime 
-import board
-import neopixel
+from color import colors
+import datetime, board, neopixel
  
 #pixel_pin = board.D18
-num_pixels = 160
+#num_pixels = 16
+#pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False)
 
-
-#COLORS
-colors = {
-  "BLACK":(0, 0, 0),
-  "RED":(255, 0, 0), 
-  "YELLOW": (255, 150, 0),
-  "GREEN":(0, 255, 0),
-  "CYAN":(0, 255, 255),
-  "BLUE":(0, 0, 255),
-  "PURPLE":(180, 0, 255)
-}
 #CELLS
 c16 = {
   "C1.6.1":range(152, 160),
@@ -37,7 +26,26 @@ c16 = {
   "C1.6.15":range(12, 20),
   "C1.6.16":range(2, 10) 
   }
-#pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False)
+
+  c17 = {
+  "C1.7.1":range(152, 160),
+  "C1.7.2":range(142, 150),
+  "C1.7.3":range(132, 140),
+  "C1.7.4":range(122, 130),
+  "C1.7.5":range(112, 120),
+  "C1.7.6":range(102, 110),
+  "C1.7.7":range(92, 100),
+  "C1.7.8":range(82, 90),
+  "C1.7.9":range(72, 80),
+  "C1.7.10":range(62, 70),
+  "C1.7.11":range(52, 60),
+  "C1.7.12":range(42, 50),
+  "C1.7.13":range(32, 40),
+  "C1.7.14":range(22, 30),
+  "C1.7.15":range(12, 20),
+  "C1.7.16":range(2, 10)
+  }
+
 
 
 app = Flask(__name__)
@@ -70,7 +78,16 @@ def test(cell, color):
           pixels[i] = colors[c]
           pixels.show()
       else:
-         return 'Cell not found' 
+        return 'Looking on another PIN'
+        if ce in c17:
+        pixel_pin = board.D21
+        pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False)
+        for i in c17[ce]:
+          pixels[i] = colors[c]
+          pixels.show()
+         else:
+           "No cell found"
+          
     else:
       return "Color not found"  
     return 'Current cell is %s' % escape(ce)
